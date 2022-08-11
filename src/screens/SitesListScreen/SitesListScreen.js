1a5736fc72733709a04e56562865f8c68772addc
banchi <icaro_19@hotmail.com>
2022-08-11 13:21:05 +0200

    Add SiteListScreen component

--- src/screens/SitesListScreen/SitesListScreen.js
+++ src/screens/SitesListScreen/SitesListScreen.js
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import CustomCard from "../../components/CustomCard.js/CustomCard"
+import { getSites } from "../../redux/actions/sitesActions"
+import { ContainerSitesList } from "./styles/sitesListScreenStyled"
+
 const SitesListScreen = () => {
-	return (
-		<>
-			<h1>
-				sites list
-			</h1>
-		</>
-	)
+  const dispatch = useDispatch()
+
+  const [currentSites, setCurrentSites] = useState([])
+  const { sites } = useSelector((state) => state.sitesList)
+
+  useEffect(() => {
+    dispatch(getSites())
+  }, [dispatch])
+  useEffect(() => {
+    if (sites) {
+      setCurrentSites(sites)
+    }
+  }, [sites])
+
+
+  return (
+    <ContainerSitesList>
+      {currentSites?.map((site) => {
+        return (
+          <CustomCard key={site._id} site={site} />
+        )
+      })}
+    </ContainerSitesList>
+  )
 }
 export default SitesListScreen
--- src/screens/SitesListScreen/styles/sitesListScreenStyled.js
+++ src/screens/SitesListScreen/styles/sitesListScreenStyled.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
 
 export const Header = styled.div`
-	border: '1px solid green' ;
+`
+
+export const ContainerSitesList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  h2{
+    padding: 10px 0px 10px 0px;
+  }
+  article{
+    padding: 10px 0px 10px 0px;
+    span{
+      padding: 10px 0px 10px 0px;
+    }
+    a{
+      cursor: pointer;
+      text-decoration: none;
+      color: black;
+    }
+  }
+  @media only screen and (min-width: 768px){
+    border: 1px solid green;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
 `
